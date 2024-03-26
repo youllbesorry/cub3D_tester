59b7f0ae3aeaf366e981f340223759b2e4571c07
youllbesorry <51507664+youllbesorry@users.noreply.github.com>
2024-03-26 13:21:11 +0100

    translat

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # cub3D_tester
+
+This tester launch every maps in maps_test folder.
+
+If the map name start with a 'b_' it's mean it's a bad map so the tester will expect a error and a exit code.
+If the map name start with a 'g_' it's mean it's a good map so the tester will expect no error and the launch of the program then it will be timeout by the script.
+if the map name start with a 'v_' it's mean it's a valid map so the tester will expect a error and the launch of the program then it will be timeout by the script.
+
+If you dont need the last case, just remove the maps how start with a 'v_'.
+
+You can add as many test you want but you have to follow the 3 rules above in order to the script work as expected.
--- script.sh
+++ script.sh
@@ -5,42 +5,32 @@
 	compteur_invalid=0
 	# Boucle sur chaque fichier map dans le répertoire maps
 	for mapfile in maps_test/*; do
-		echo "Traitement de la map: $mapfile"
+		echo "Processing map: $mapfile"
 		# Vérifie si le nom du fichier commence par 'b'
 		 output=$(timeout --foreground 5s valgrind --leak-check=full --show-leak-kinds=all --error-exitcode=1 --log-file=/dev/null ./cub3D "$mapfile" 2>&1);
 		exit_status=$?
 		if [ $exit_status -eq 124 ]; then
-			echo -e "Le programme $mapfile a été terminé en raison d'un dépassement de temps (timeout).\n"
+			echo -e "The program $mapfile was terminated due to a timeout.\n"
 			if [[ $(basename "$mapfile") == b* ]]; then
-				# echo -e "\n"
 				echo -e "\e[31mtimeout ERROR: $mapfile\e[0m"
-				# echo -e "\n"
 				((compteur_invalid++))
 			fi
 			if [[ $(basename "$mapfile") == v* ]]; then
-				# echo -e "\n"
 				echo -e "\e[32mtimeout OK: $mapfile\e[0m"
-				# echo -e "\n"
 				((compteur_valid++))
 			fi
 		else
-			echo -e "Le programme $mapfile a été terminé avec le code de sortie $exit_status.\n"
+			echo -e "The program $mapfile was terminated with exit code $exit_status.\n"
 			if [[ $(basename "$mapfile") == b* ]]; then
-				# echo -e "\n"
-				echo -e "\e[32mProgramme exit OK: $mapfile\e[0m"
-				# echo -e "\n"
+				echo -e "\e[32mProgram exit OK: $mapfile\e[0m"
 				((compteur_valid++))
 			fi
 			if [[ $(basename "$mapfile") == v* ]]; then
-				# echo -e "\n"
-				echo -e "\e[32mProgramme exit ERROR: $mapfile\e[0m"
-				# echo -e "\n"
+				echo -e "\e[32mProgram exit ERROR: $mapfile\e[0m"
 				((compteur_invalid++))
 			fi
 			if [[ $(basename "$mapfile") == g* ]]; then
-				# echo -e "\n"
-				echo -e "\e[32mProgramme exit ERROR: $mapfile\e[0m"
-				# echo -e "\n"
+				echo -e "\e[32mProgram exit ERROR: $mapfile\e[0m"
 				((compteur_invalid++))
 			fi
 		fi
@@ -49,15 +39,11 @@
 			# Cherche le motif d'erreur spécifique "Error\n \n"
 			if echo "$output" | grep -Pzo "Error.*(\n.*\n)?"; then
 				# Si le motif d'erreur est trouvé, affiche les erreurs
-				# echo -e "\n"
 				echo -e "\e[32mOK: $mapfile\e[0m"
-				# echo -e "\n"
 				((compteur_valid++))
 			else
 				# Si aucune erreur correspondant au motif n'est trouvée et que le nom commence par 'b'
-				# echo -e "\n"
 				echo -e "\e[31mERROR: $mapfile\e[0m"
-				# echo -e "\n"
 				((compteur_invalid++))
 			fi
 		fi
@@ -65,31 +51,23 @@
 			# Exécute cub3D avec valgrind, capture la sortie standard et d'erreur
 			# Cherche le motif d'erreur spécifique "Error\n \n"
 			if echo "$output" | grep -Pzo "Error.*(\n.*)?"; then
-				# echo -e "\n"
 				echo -e "\e[31mERROR: $mapfile\e[0m"
-				# echo -e "\n"
 				((compteur_invalid++))
 			else
 				# Si aucune erreur correspondant au motif n'est trouvée et que le nom commence par 'b'
-				# echo -e "\n"
 				echo -e "\e[32mOK: $mapfile\e[0m"
-				# echo -e "\n"
 				((compteur_valid++))
 			fi
 		fi
 		if [[ $(basename "$mapfile") == v* ]]; then
 			if echo "$output" | grep -Pzo "Error.*(\n.*\n)?"; then
-				# echo -e "\n"
 				echo -e "\e[32mOK: $mapfile\e[0m"
-				# echo -e "\n"
 			else
-				# echo -e ZzzZ"\n"
 				echo -e "\e[31mERROR: $mapfile\e[0m"
-				# echo -e "\n"
 			fi
 		fi
 	done
-	echo -e "\e[32mNombre de maps valides: $compteur_valid\e[0m"
-	echo -e "\e[31mNombre de maps invalides: $compteur_invalid\e[0m"
+	echo -e "\e[32mNumber of valid map: $compteur_valid\e[0m"
+	echo -e "\e[31mNumber of invalid map: $compteur_invalid\e[0m"
 } 2>&1 | tee outfile.txt
 
